7b1e46b4fdcf2357fb6647f8631897e8c21ef181
ElvisMw <elvismwanthi@gmail.com>
2024-04-25 16:56:02 +0300

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """
+    Computes the n-th row of Pascal's triangle.
+
+    Parameters
+    ----------
+    n : int
+        The row number, 0-indexed.
+
+    Returns
+    -------
+    list
+        The n-th row of Pascal's triangle.
+
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        """ Compute the i-th row of Pascal's triangle """
+
+        row = [1]
+        for j in range(1, i):
+            """ The (i, j)-th entry of Pascal's triangle is the sum of the (i-1, j-1)
+            and (i-1, j) entries """
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
