97cf4e41830ff995c88167ea4f76a28bf310f0e2
ElvisMw <elvismwanthi@gmail.com>
2024-06-06 15:57:18 +0300

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,61 +1,36 @@
 #!/usr/bin/node
 
-// Import the request module
 const request = require('request');
 
-// Retrieve the movie ID from command-line arguments
-const movieId = process.argv[2];
+// Parse movie ID from command-line arguments
+const movieID = process.argv[2];
 
-// Check if the movie ID is provided as an argument,
-// if not, display usage instructions and exit
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
+// Construct URL for movie information
+const movieURL = `https://swapi-api.alx-tools.com/api/films/${movieID}/`;
 
-// Construct the URL to fetch movie data from the Star Wars API
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-// Make an HTTP GET request to the Star Wars API to fetch movie data
-request(apiUrl, (error, response, body) => {
-  // Handle any errors that may occur during the request
+// Make HTTP GET request to fetch movie information
+request(movieURL, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-    process.exit(1);
+    console.error('Error fetching movie information:', error);
+    return;
   }
 
-  // Check if the request was successful (status code 200)
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch movie data:', response.statusCode);
-    process.exit(1);
-  }
+  // Parse JSON response
+  const movieData = JSON.parse(body);
 
-  // Parse the JSON response body to extract movie data
-  const filmData = JSON.parse(body);
-  // Extract the list of characters from the movie data
-  const characters = filmData.characters;
+  // Extract list of characters associated with the movie
+  const characters = movieData.characters;
 
-  // Iterate over each character URL in the list
-  characters.forEach(characterUrl => {
-    // Make an HTTP GET request to fetch data for each character
-    request(characterUrl, (error, response, body) => {
-      // Handle any errors that may occur during the request
+  // Iterate over the characters and display their names
+  characters.forEach(characterURL => {
+    request(characterURL, (error, response, body) => {
       if (error) {
-        console.error('Error:', error);
+        console.error('Error fetching character information:', error);
         return;
       }
 
-      // Check if the request was successful (status code 200)
-      if (response.statusCode !== 200) {
-        console.error('Failed to fetch character data:', response.statusCode);
-        return;
-      }
-
-      // Parse the JSON response body to extract character data
       const characterData = JSON.parse(body);
-      // Display the name of the character
       console.log(characterData.name);
     });
   });
 });
-
