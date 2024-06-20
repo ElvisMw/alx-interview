509095e23a9430b5569133c9b0b9aedc9c883c08
ElvisMw <elvismwanthi@gmail.com>
2024-06-20 23:23:34 +0300

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,24 +6,16 @@ from typing import List
 def makeChange(coins: List[int], total: int) -> int:
     """
     Determine the fewest number of coins needed to meet a
-	given total amount.
+    given total amount.
 
     :param coins: List of the values of the coins in your possession
     :param total: The total amount to meet with coins
     :return: Fewest number of coins needed to meet total, or
-	-1 if it's not possible
+    -1 if it's not possible
     """
-    if total <= 0:
-        return 0
-
-    """ Initialize DP array with an initial value of total
-	+ 1, which is impossible high """
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    """ Populate the DP array """
+    coins.sort(reverse=True)
+    dp = [0] + [float('inf')] * total
     for coin in coins:
         for x in range(coin, total + 1):
             dp[x] = min(dp[x], dp[x - coin] + 1)
-
     return dp[total] if dp[total] != float('inf') else -1
