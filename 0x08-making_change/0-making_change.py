cc113ac6f4ab882c6ef293797cf2d737f0b9fca4
ElvisMw <elvismwanthi@gmail.com>
2024-06-20 23:13:49 +0300

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -21,10 +21,9 @@ def makeChange(coins: List[int], total: int) -> int:
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    # Populate the DP array
+    """ Populate the DP array """
     for coin in coins:
         for x in range(coin, total + 1):
             dp[x] = min(dp[x], dp[x - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
-
