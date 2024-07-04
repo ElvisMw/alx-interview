cc62ba9e2aa31cb7044d1eed5a48ad5d433b47d9
ElvisMw <elvismwanthi@gmail.com>
2024-07-04 09:14:55 +0000

    prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+def sieve(n):
+    """ Generate list of primes up to n using the Sieve of Eratosthenes """
+    is_prime = [True] * (n + 1)
+    p = 2
+    while (p * p <= n):
+        if (is_prime[p] == True):
+            for i in range(p * p, n + 1, p):
+                is_prime[i] = False
+        p += 1
+    return [p for p in range(2, n + 1) if is_prime[p]]
+
+def isWinner(x, nums):
+    if not nums or x < 1:
+        return None
+    
+    max_num = max(nums)
+    primes = sieve(max_num)
+    
+    def count_primes_up_to(n):
+        count = 0
+        for prime in primes:
+            if prime <= n:
+                count += 1
+            else:
+                break
+        return count
+    
+    maria_wins = 0
+    ben_wins = 0
+    
+    for n in nums:
+        prime_count = count_primes_up_to(n)
+        if prime_count % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+    
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
+
+# Example Usage
+if __name__ == "__main__":
+    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+
